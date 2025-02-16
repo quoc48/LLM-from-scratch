2c0c1b630632e138755f74976eacde5fbd605935
quoc48 <thanh.quoc731@gmail.com>
2025-02-17 06:13:00 +0700

    work with pytorch

--- ch02/ch02.py
+++ ch02/ch02.py
@@ -1,6 +1,13 @@
 import os
 import urllib.request
+from importlib.metadata import version
+import tiktoken
+import torch
+from torch.utils.data import Dataset, DataLoader
 
+import re
+
+# Check file existing?
 if not os.path.exists("the-verdict.txt"):
     url = ("https://raw.githubusercontent.com/rasbt/"
            "LLMs-from-scratch/main/ch02/01_main-chapter-code/"
@@ -8,7 +15,65 @@ if not os.path.exists("the-verdict.txt"):
     file_path = "the-verdict.txt"
     urllib.request.urlretrieve(url, file_path)
 
+class SimpleTokenizerV2:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = { i:s for s,i in vocab.items()}
+    
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [
+            item if item in self.str_to_int 
+            else "<|unk|>" for item in preprocessed
+        ]
+
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+        
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        # Replace spaces before the specified punctuations
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
+        return text
+
+
+# Byte pair encoding
+tokenizer = tiktoken.get_encoding("gpt2")
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
-print("Total number of character:", len(raw_text))
-print(raw_text[:99])
+
+enc_text = tokenizer.encode(raw_text)
+print(len(enc_text))
+
+enc_sample = enc_text[50:]
+
+context_size = 4  # determine how many tokens are included in the input
+for i in range(1, context_size+1):
+    context = enc_sample[:i]
+    desired = enc_sample[i]
+    print(tokenizer.decode(context), "---->", tokenizer.decode([desired]))
+
+# A dataset for batched inputs and targets
+class GPTDatasetV1(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        # Tokenize the entire text
+        token_ids = tokenizer.encode(txt, allowed_special={"<|endoftext|>"})
+
+        # Use a sliding window to chunk the book into overlapping sequence of max_length
+        for i in range(0,len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i:i + max_length]
+            target_chunk = token_ids[i + 1: i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    # Return the total number of row in the dataset    
+    def __len__(self):
+        return len(self.input_ids)
+    
+    # Return a single row from the dataset
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]

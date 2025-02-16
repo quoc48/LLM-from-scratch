2a114d54f35a841cdbbb7fcbefbd08eea21fa1fe
quoc48 <thanh.quoc731@gmail.com>
2025-02-16 11:45:41 +0700

    check text file

---
+++ ch02/ch02.py
@@ -0,0 +1,14 @@
+import os
+import urllib.request
+
+if not os.path.exists("the-verdict.txt"):
+    url = ("https://raw.githubusercontent.com/rasbt/"
+           "LLMs-from-scratch/main/ch02/01_main-chapter-code/"
+           "the-verdict.txt")
+    file_path = "the-verdict.txt"
+    urllib.request.urlretrieve(url, file_path)
+
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+print("Total number of character:", len(raw_text))
+print(raw_text[:99])

95fda233c2d4422c6c689b85ea9910fcefc71fb9
quoc48 <thanh.quoc731@gmail.com>
2025-02-19 22:48:45 +0700

    test

--- ch02/test-computational-graph.py
+++ ch02/test-computational-graph.py
@@ -1,19 +1,57 @@
-import torch.nn.functional as F
-from torch.autograd import grad
 import torch
+from torch.utils.data import Dataset, DataLoader
+import os
 
-y = torch.tensor([1.0])
-x1 = torch.tensor([1.1])
-w1 = torch.tensor([2.2], requires_grad=True)
-b = torch.tensor([0.0], requires_grad=True)
+# Create a small toy dataset
+X_train = ([
+    [-1.2, 3.1],
+    [-0.9, 2.9],
+    [-0.5, 2.6],
+    [2.3, -1.1],
+    [2.7, -1.5]
+])
 
-z = x1 * w1 + b
-a = torch.sigmoid(z)
+y_train = torch.tensor([0, 0, 0, 1, 1])
 
-loss = F.binary_cross_entropy(a, y)
+X_test = torch.tensor([
+[-0.8, 2.8],
+[2.6, -1.6],
+])
+y_test = torch.tensor([0, 1])
 
-grad_L_w1 = grad(loss, w1, retain_graph=True)
-grad_L_b = grad(loss, b, retain_graph=True)
+class ToyDataset(Dataset):
+    def __init__(self, X, y):
+        self.features = X
+        self.labels = y
+    def __getitem__(self, index):
+        one_x = self.features[index]
+        one_y = self.labels[index]
+        return one_x, one_y
+    def __len__(self):
+        return self.labels.shape[0]
 
-print(grad_L_w1)
-print(grad_L_b)
+train_ds = ToyDataset(X_train, y_train)
+test_ds = ToyDataset(X_test, y_test)
+
+print(len(train_ds))
+
+# Dataloader
+torch.manual_seed(123)
+
+train_loader = DataLoader(
+    dataset=train_ds,
+    batch_size=2,
+    shuffle=True,
+    num_workers=0,
+    drop_last=True
+)
+
+test_loader = DataLoader(
+    dataset=test_ds,
+    batch_size=2,
+    shuffle=False,
+    num_workers=0
+)
+
+for idx, (x, y) in enumerate(train_loader):
+    print(f"Batch {idx+1}:", x, y)

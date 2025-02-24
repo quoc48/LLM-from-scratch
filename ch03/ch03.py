f8ffa40025a0635b14eda8860eb699b5233c42d9
quoc48 <thanh.quoc731@gmail.com>
2025-02-25 06:00:24 +0700

    adding causal

--- ch03/ch03.py
+++ ch03/ch03.py
@@ -34,7 +34,7 @@ x_2 = inputs[1] # second input element
 d_in = inputs.shape[1] # input embedding size. d=3
 d_out = 2   # the output embedding size, d=2
 
-# initialize three weight matices
+# initialize three weight matrices
 torch.manual_seed(123)
 W_query = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
 W_key   = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
@@ -108,3 +108,50 @@ class SelfAttention_v2(nn.Module):
 torch.manual_seed(789)
 sa_v1 = SelfAttention_v2(d_in, d_out)
 print(sa_v1(inputs))
+
+#_____________________________________________
+# Causal class
+class CausalAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_length,
+                 dropout, qkv_bias=False):
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key   = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = nn.Dropout(dropout) # New
+        self.register_buffer(
+            'mask',
+            torch.triu(torch.ones(context_length, context_length),
+            diagonal=1)
+        ) # New
+    
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape # New batch dimension b
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        attn_scores = queries @ keys.transpose(1, 2) # Changed transpose
+        attn_scores.masked_fill_(  # New, _ ops are in-place
+            self.mask.bool()[:num_tokens, :num_tokens], -torch.inf)  # `:num_tokens` to account for cases where the number of tokens in the batch is smaller than the supported context_size
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+        )
+        attn_weights = self.dropout(attn_weights) # New
+
+        context_vec = attn_weights @ values
+        return context_vec
+
+batch = torch.stack((inputs, inputs), dim=0)
+print(batch.shape) # 2 inputs with 6 tokens each, and each token has embedding dimension 3
+
+torch.manual_seed(123)
+
+context_length = batch.shape[1]
+ca = CausalAttention(d_in, d_out, context_length, 0.0)
+
+context_vecs = ca(batch)
+
+print(context_vecs)
+print("context_vecs.shape:", context_vecs.shape)

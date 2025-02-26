955c1dc2bb2dd805cbfc2cb105da3a467c6b9f01
quoc48 <thanh.quoc731@gmail.com>
2025-02-26 22:51:03 +0700

    adding batched matrix multiplications

--- ch03/ch03.py
+++ ch03/ch03.py
@@ -13,20 +13,20 @@ inputs = torch.tensor(
 # Step 1: compute all attention scores
 attn_scores = torch.empty(6,6)
 attn_scores = inputs @ inputs.T # Replace the for loop in Python for inceasing performance
-print(attn_scores)
+#print(attn_scores)
 
 # Step 2: compute all attention weights
 attn_weights = torch.softmax(attn_scores, dim=-1) # dim= -1 or 1 will apply on each row
-print(attn_weights)
+#print(attn_weights)
 
 # Verify the row indeeded sum to 1
 row_2_sum = sum([0.1385, 0.2379, 0.2333, 0.1240, 0.1082, 0.1581])
-print("Row 2 sum:", row_2_sum)
-print("All row sums:", attn_weights.sum(dim=-1))
+#print("Row 2 sum:", row_2_sum)
+#print("All row sums:", attn_weights.sum(dim=-1))
 
 # Step 3: compute all context vector
 all_context_vecs = attn_weights @ inputs
-print(all_context_vecs)
+#print(all_context_vecs)
 
 #_______________________________________________________
 
@@ -45,24 +45,24 @@ query_2 = x_2 @ W_query # _2 because it's with respect to the 2nd input element
 key_2 = x_2 @ W_key 
 value_2 = x_2 @ W_value
 
-print(query_2)
+#print(query_2)
 keys = inputs @ W_key
 values = inputs @ W_value
-print("keys.shape: ", keys.shape)
-print("values.shape: ", values.shape)
+#print("keys.shape: ", keys.shape)
+#print("values.shape: ", values.shape)
 
 # Step 1
 attn_scores_2 = query_2 @ keys.T # All attention scores for given query
-print(attn_scores_2)
+#print(attn_scores_2)
 
 # Step 2
 d_k = keys.shape[1]
 attn_weights_2 = torch.softmax(attn_scores_2 / d_k**0.5, dim=-1)
-print(attn_weights_2)
+#print(attn_weights_2)
 
 # Step 3
 context_vec_2 = attn_weights_2 @ values
-print(context_vec_2)
+#print(context_vec_2)
 
 #______________________________________________________________
 
@@ -107,7 +107,7 @@ class SelfAttention_v2(nn.Module):
 
 torch.manual_seed(789)
 sa_v1 = SelfAttention_v2(d_in, d_out)
-print(sa_v1(inputs))
+#print(sa_v1(inputs))
 
 #_____________________________________________
 # Causal class
@@ -144,7 +144,7 @@ class CausalAttention(nn.Module):
         return context_vec
 
 batch = torch.stack((inputs, inputs), dim=0)
-print(batch.shape) # 2 inputs with 6 tokens each, and each token has embedding dimension 3
+#print(batch.shape) # 2 inputs with 6 tokens each, and each token has embedding dimension 3
 
 #__________________________________________________
 # Multi-head attentions
@@ -168,6 +168,79 @@ d_in, d_out = 3, 2
 mha = MultiHeadAttentionWrapper(
     d_in, d_out, context_length, 0.0, num_heads=2
 )
+
+context_vecs = mha(batch)
+
+#print(context_vecs)
+#print("context_vecs.shape:", context_vecs.shape)
+
+#__________________________________________________
+# Multi-head attentions with weight splits
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        assert (d_out % num_heads == 0), \
+            "d_out must be divisible by num_heads"
+
+        self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads # Reduce the projection dim to match desired output dim
+
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.out_proj = nn.Linear(d_out, d_out)  # Linear layer to combine head outputs
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer(
+            "mask",
+            torch.triu(torch.ones(context_length, context_length),
+                       diagonal=1)
+        )
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+
+        keys = self.W_key(x) # Shape: (b, num_tokens, d_out)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        # We implicitly split the matrix by adding a `num_heads` dimension
+        # Unroll last dim: (b, num_tokens, d_out) -> (b, num_tokens, num_heads, head_dim)
+        keys = keys.view(b, num_tokens, self.num_heads, self.head_dim) 
+        values = values.view(b, num_tokens, self.num_heads, self.head_dim)
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+
+        # Transpose: (b, num_tokens, num_heads, head_dim) -> (b, num_heads, num_tokens, head_dim)
+        keys = keys.transpose(1, 2)
+        queries = queries.transpose(1, 2)
+        values = values.transpose(1, 2)
+
+        # Compute scaled dot-product attention (aka self-attention) with a causal mask
+        attn_scores = queries @ keys.transpose(2, 3)  # Dot product for each head
+
+        # Original mask truncated to the number of tokens and converted to boolean
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
+
+        # Use the mask to fill attention scores
+        attn_scores.masked_fill_(mask_bool, -torch.inf)
+        
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+
+        # Shape: (b, num_tokens, num_heads, head_dim)
+        context_vec = (attn_weights @ values).transpose(1, 2) 
+        
+        # Combine heads, where self.d_out = self.num_heads * self.head_dim
+        context_vec = context_vec.contiguous().view(b, num_tokens, self.d_out)
+        context_vec = self.out_proj(context_vec) # optional projection
+
+        return context_vec
+    
+torch.manual_seed(123)
+
+batch_size, context_length, d_in = batch.shape
+d_out = 2
+mha = MultiHeadAttention(d_in, d_out, context_length, 0.0, num_heads=2)
 
 context_vecs = mha(batch)
 

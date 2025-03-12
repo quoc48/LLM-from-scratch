61a2c036f863224f1b82abd7301cbfa6626ff45a
quoc48 <thanh.quoc731@gmail.com>
2025-03-12 22:52:16 +0700

    cross entropy loss

--- ch05/ch05.py
+++ ch05/ch05.py
@@ -43,4 +43,48 @@ token_ids = generate_text_simple(
     context_size=GPT_CONFIG_124M["context_length"]
 )
 
-print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+#print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
+# calculating the text generation loss
+inputs = torch.tensor([[16833, 3626, 6100],   # ["every effort moves",
+                       [40,    1107, 588]])   #  "I really like"]
+
+targets = torch.tensor([[3626, 6100, 345  ],  # [" effort moves you",
+                        [1107,  588, 11311]]) #  " really like chocolate"]
+
+with torch.no_grad():
+    logits = model(inputs)
+
+probas = torch.softmax(logits, dim=-1) # Probability of each token in vocabulary
+#print(probas.shape) # Shape: (batch_size, num_tokens, vocab_size)
+
+token_ids = torch.argmax(probas, dim=-1, keepdim=True)
+#print("Token IDs:\n", token_ids)
+
+#print(f"Targets batch 1: {token_ids_to_text(targets[0], tokenizer)}")
+#print(f"Outputs batch 1: {token_ids_to_text(token_ids[0].flatten(), tokenizer)}")
+
+#_________________
+text_idx = 0
+target_probas_1 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+print("Text 1:", target_probas_1)
+
+text_idx = 1
+target_probas_2 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+print("Text 2:", target_probas_2)
+
+# Compute logarithm of all token probabilities
+log_probas = torch.log(torch.cat((target_probas_1, target_probas_2)))
+print(log_probas)
+
+# Calculate the average probability for each token
+avg_log_probas = torch.mean(log_probas)
+print(avg_log_probas)
+
+logits_flat = logits.flatten(0, 1)
+targets_flat = targets.flatten()
+
+print("Flattened logits:", logits_flat.shape)
+print("Flattened targets:", targets_flat.shape)
+loss = torch.nn.functional.cross_entropy(logits_flat, targets_flat)
+print(loss)

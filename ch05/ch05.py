17a87d79322bdc6531b199f6a7764374a03e2962
quoc48 <thanh.quoc731@gmail.com>
2025-03-15 15:17:54 +0700

    Training and validation losses

--- ch05/ch05.py
+++ ch05/ch05.py
@@ -67,24 +67,127 @@ token_ids = torch.argmax(probas, dim=-1, keepdim=True)
 #_________________
 text_idx = 0
 target_probas_1 = probas[text_idx, [0, 1, 2], targets[text_idx]]
-print("Text 1:", target_probas_1)
+#print("Text 1:", target_probas_1)
 
 text_idx = 1
 target_probas_2 = probas[text_idx, [0, 1, 2], targets[text_idx]]
-print("Text 2:", target_probas_2)
+#print("Text 2:", target_probas_2)
 
 # Compute logarithm of all token probabilities
 log_probas = torch.log(torch.cat((target_probas_1, target_probas_2)))
-print(log_probas)
+#print(log_probas)
 
 # Calculate the average probability for each token
 avg_log_probas = torch.mean(log_probas)
-print(avg_log_probas)
+#print(avg_log_probas)
 
 logits_flat = logits.flatten(0, 1)
 targets_flat = targets.flatten()
 
-print("Flattened logits:", logits_flat.shape)
-print("Flattened targets:", targets_flat.shape)
+#print("Flattened logits:", logits_flat.shape)
+#print("Flattened targets:", targets_flat.shape)
 loss = torch.nn.functional.cross_entropy(logits_flat, targets_flat)
-print(loss)
+#print(loss)
+
+#____________ Training and validation losses
+import os
+import urllib.request
+
+file_path = "the-verdict.txt"
+url = "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch02/01_main-chapter-code/the-verdict.txt"
+
+if not os.path.exists(file_path):
+    with urllib.request.urlopen(url) as response:
+        text_data = response.read().decode('utf-8')
+    with open(file_path, "w", encoding="utf-8") as file:
+        file.write(text_data)
+else:
+    with open(file_path, "r", encoding="utf-8") as file:
+        text_data = file.read()
+
+# First 100 characters
+#print(text_data[:99])
+
+# Last 100 characters
+#print(text_data[-99:])
+
+total_characters = len(text_data)
+total_tokens = len(tokenizer.encode(text_data))
+
+print("Characters:", total_characters)
+print("Tokens:", total_tokens)
+
+#__________
+from ch02.ch02 import create_dataloader_v1
+
+# Train/validation ratio
+train_ratio = 0.90
+split_idx = int(train_ratio * len(text_data))
+train_data = text_data[:split_idx]
+val_data = text_data[split_idx:]
+
+torch.manual_seed(123)
+
+train_loader = create_dataloader_v1(
+    train_data,
+    batch_size=2,
+    max_length=GPT_CONFIG_124M["context_length"],
+    stride=GPT_CONFIG_124M["context_length"],
+    drop_last=True,
+    shuffle=True,
+    num_workers=0
+)
+
+val_loader = create_dataloader_v1(
+    val_data,
+    max_length=GPT_CONFIG_124M["context_length"],
+    stride=GPT_CONFIG_124M["context_length"],
+    drop_last=False,
+    shuffle=False,
+    num_workers=0
+)
+
+print("Train loader:")
+for x, y in train_loader:
+    print(x.shape, y.shape)
+
+print("\nValidation loader:")
+for x, y in val_loader:
+    print(x.shape, y.shape)
+
+
+def calc_loss_batch(input_batch, target_batch, model, device):
+    input_batch, target_batch = input_batch.to(device), target_batch.to(device)
+    logits = model(input_batch)
+    loss = torch.nn.functional.cross_entropy(logits.flatten(0, 1), target_batch.flatten())
+    return loss
+
+def calc_loss_loader(data_loader, model, device, num_batches=None):
+    total_loss = 0.
+    if len(data_loader) == 0:
+        return float("nan")
+    elif num_batches is None:
+        num_batches = len(data_loader)
+    else:
+        # Reduce the number of batches to match the total number of batches in the data loader
+        # if num_batches exceeds the number of batches in the data loader
+        num_batches = min(num_batches, len(data_loader))
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+        if i < num_batches:
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            total_loss += loss.item()
+        else:
+            break
+    return total_loss / num_batches # Averages the loss over all batches
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device) # no assignment model = model.to(device) necessary for nn.Module classes
+torch.manual_seed(123) # For reproducibility due to the shuffling in the data loader
+
+with torch.no_grad(): # Disable gradient tracking for efficiency because we are not training, yet
+    train_loss = calc_loss_loader(train_loader, model, device)
+    val_loss = calc_loss_loader(val_loader, model, device)
+
+print("Training loss:", train_loss)
+print("Validation loss:", val_loss)
+
